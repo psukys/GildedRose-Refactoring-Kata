41f4622a58132663013bcaf4e9eb7f9666f96022
Paulius Šukys <paul.sukys@gmail.com>
2019-04-13 09:17:43 +0300

    Break into per-item switch-cases: - sulfuras still skips the update - every item has sellin degrade - check of quality [0,50] range for all

--- php/src/gilded_rose.php
+++ php/src/gilded_rose.php
@@ -10,39 +10,39 @@ class GildedRose {
 
     function update_quality() {
         foreach ($this->items as $item) {
-            if ($item->name == 'Sulfuras, Hand of Ragnaros') {
+            if ($item->name == 'Sulfuras, Hand of Ragnaros') { // special case - no changes
                 continue;
             }
 
-            // normal item degrade
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                $item->quality = $item->quality - 1;
-            // brie or concert upgrade
-            } else {
-                $item->quality = $item->quality + 1; // brie would end here
-                if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($item->sell_in <= 10) {
-                        $item->quality = $item->quality + 1;
+            switch ($item->name) {
+                case 'Aged Brie':
+                    if ($item->sell_in > 0) {
+                        $item->quality += 1;
+                    } else {
+                        $item->quality += 2; // expired 2x
                     }
-                    if ($item->sell_in <= 5) {
-                        $item->quality = $item->quality + 1;
+                    break;
+                case 'Backstage passes to a TAFKAL80ETC concert':
+                    if ($item->sell_in <= 0) { // expired
+                        $item->quality = 0;
+                    } elseif ($item->sell_in <= 5) { // 3x 5 days before
+                        $item->quality += 3;
+                    } elseif ($item->sell_in <= 10) { // 2x 10 days before
+                        $item->quality += 2;
+                    } else {
+                        $item->quality += 1;
                     }
-                }
+                    break;
+                default: // normal items
+                    if ($item->sell_in > 0) {
+                        $item->quality -= 1;
+                    } else {
+                        $item->quality -= 2;
+                    }
             }
-            
-            // sellin degrade - can be negative!
-            $item->sell_in = $item->sell_in - 1;
-            
-            if ($item->sell_in < 0) {
-                if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    $item->quality = 0; // after concert -> 0
-                }
-                if ($item->name != 'Aged Brie') {
-                    $item->quality = $item->quality - 1; // normal expired item -2 (-1 before)
-                } else {
-                    $item->quality = $item->quality + 1; // undocumented -> expired upgrades twice
-                }
-            }
+
+            $item->sell_in -= 1;
+
             if ($item->quality > 50) {
                 $item->quality = 50;
             }
